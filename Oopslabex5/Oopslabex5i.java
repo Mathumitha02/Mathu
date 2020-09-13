e852d151ba094d9ee910a52fb274889819a2192f
Mathumitha02 <69072433+Mathumitha02@users.noreply.github.com>
2020-09-13 20:59:55 +0530

    Add files via upload

---
+++ Oopslabex5/Oopslabex5i.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mathu
+ */
+class Degree
+{
+    public void getDegree()
+    {
+        System.out.println("I got a degree");
+    }
+}
+class Undergraduate extends Degree
+{
+    public void getDegree()
+    {
+        System.out.println("I am an Undergraduate");
+    }
+}
+class Postgraduate extends Degree
+{
+    public void getDegree()
+    {
+        System.out.println("I am a Postgraduate");
+    }
+}
+class oopslabex5i
+{
+    public static void main(String[]args)
+    {
+        Undergraduate a=new Undergraduate();
+        Postgraduate b=new Postgraduate();
+        a.getDegree();
+        b.getDegree();
+    }
+}
+    
+
+
