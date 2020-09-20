0ffb0d4e309db72edd54baa08d33850893dcf67a
Mathumitha02 <69072433+Mathumitha02@users.noreply.github.com>
2020-09-20 19:36:07 +0530

    Add files via upload

---
+++ Oopslabex(1-7)/Oopslabex04/A.java
@@ -0,0 +1,5 @@
+ package pack;
+public class A{
+ public void met1(){
+	System.out.println("hello");
+}
---
+++ Oopslabex(1-7)/Oopslabex04/Programii.java
@@ -0,0 +1,9 @@
+import com.*;
+packae mypackage;
+public class programii{
+public static void main(String[] args){
+loanaccount acc=new loanaccount();
+transact acc=new credit();
+transact acc=new debit();
+}
+
